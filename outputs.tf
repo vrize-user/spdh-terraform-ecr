ba64d544f18f4bd6a11f20a8ee3b96344874b10e
vrize-user <jalandhar@vrize.com>
2024-08-26 14:49:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ecr_repository_url" {
   description = "The URL of the ECR repository"
-  value       = aws_ecr_repository.myapp.repository_url
+  value       = aws_ecr_repository.my_ecr_repo.repository_url
 }
