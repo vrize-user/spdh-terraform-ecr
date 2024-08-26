c28bec574691107591958aaaf986dbd6ea388f49
vrize-user <jalandhar@vrize.com>
2024-08-26 14:51:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 # Create ECR repository
 resource "aws_ecr_repository" "my_ecr_repo" {
-  name = "myapp"
+  name = "my_ecr_repo"
 }
 
 # Outputs for the repository URL
 output "repository_url" {
-  value = aws_ecr_repository.myapp.repository_url
+  value = aws_ecr_repository.my_ecr_repo.repository_url
 }
